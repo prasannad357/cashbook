ae20c0ea40ab8a6ea4d9e1dc7f67dfb7f849979b
prasannad357 <59197308+prasannad357@users.noreply.github.com>
2024-06-30 21:34:08 +0530

    - Wrote tests for CashbookDao and TransactionDao

---
+++ app/src/androidTest/java/com/prasanna/cashbook/feature_cashbook/data/data_source/CashbookDaoTest.kt
@@ -0,0 +1,161 @@
+package com.prasanna.cashbook.feature_cashbook.data.data_source
+
+import android.content.Context
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
+import app.cash.turbine.test
+import com.prasanna.cashbook.feature_cashbook.domain.model.Cashbook
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runTest
+import org.junit.After
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.time.LocalDate
+
+
+@RunWith(AndroidJUnit4::class)
+@SmallTest
+class CashbookDaoTest {
+
+    lateinit var db:CashbookDatabase
+    lateinit var dao:CashbookDao
+    @Before
+    fun setUp(){
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        db = Room.inMemoryDatabaseBuilder(context, CashbookDatabase::class.java).allowMainThreadQueries().build()
+        dao = db.cashbookDao
+    }
+
+    @After
+    fun tearDown(){
+        db.close()
+    }
+
+
+    @Test
+    fun addCashbook_addsTwo_returnsSize() = runTest{
+        val cashbook = Cashbook(
+            name = "TestBook",
+            tags = emptyList(),
+            createdDate = LocalDate.now(),
+            createdTimeStamp = 1L,
+            lastEditTimeStamp = 2L,
+            id = 1
+        )
+        val cashbook2 = Cashbook(
+            name = "TestBook2",
+            tags = emptyList(),
+            createdDate = LocalDate.now(),
+            createdTimeStamp = 1L,
+            lastEditTimeStamp = 2L,
+            id = 2
+        )
+        dao.addCashbook(cashbook)
+        dao.addCashbook(cashbook2)
+        dao.getCashbooks().test{
+            val result = awaitItem()
+            Assert.assertEquals(2, result.size)
+            Assert.assertEquals(true, result.contains(cashbook))
+            Assert.assertEquals(true, result.contains(cashbook2))
+            cancel()
+        }
+
+    }
+
+    @Test
+    fun getCashbookById_whenIdPassed_returnsCashbook() = runTest{
+        val cashbook = Cashbook(
+            name = "TestBook",
+            tags = emptyList(),
+            createdDate = LocalDate.now(),
+            createdTimeStamp = 1L,
+            lastEditTimeStamp = 2L,
+            id = 1
+        )
+        dao.addCashbook(cashbook)
+        val result = dao.getCashbookById(cashbook.id!!)
+        Assert.assertEquals(cashbook, result)
+    }
+
+    @Test
+    fun getCashbookById_whenInvalidId_returnsNull() = runTest {
+        val cashbook = Cashbook(
+            name = "TestBook",
+            tags = emptyList(),
+            createdDate = LocalDate.now(),
+            createdTimeStamp = 1L,
+            lastEditTimeStamp = 2L,
+            id = 1
+        )
+        dao.addCashbook(cashbook)
+
+        val result = dao.getCashbookById(2)
+        Assert.assertEquals(null, result)
+    }
+
+    @Test
+    fun deleteCashbook_whenValidCashbook_deletesCashbook() = runTest {
+        val cashbook = Cashbook(
+            name = "TestBook",
+            tags = emptyList(),
+            createdDate = LocalDate.now(),
+            createdTimeStamp = 1L,
+            lastEditTimeStamp = 2L,
+            id = 1
+        )
+        val cashbook2 = Cashbook(
+            name = "TestBook2",
+            tags = emptyList(),
+            createdDate = LocalDate.now(),
+            createdTimeStamp = 1L,
+            lastEditTimeStamp = 2L,
+            id = 2
+        )
+        dao.addCashbook(cashbook)
+        dao.addCashbook(cashbook2)
+
+        dao.deleteCashbook(cashbook)
+
+        dao.getCashbooks().test {
+            val result = awaitItem()
+            Assert.assertEquals(false, result.contains(cashbook))
+            Assert.assertEquals(1, result.size)
+            cancel()
+        }
+    }
+
+    @Test
+    fun deleteCashbook_invalidCashbook_nothingDeleted() = runTest {
+        val cashbook = Cashbook(
+            name = "TestBook",
+            tags = emptyList(),
+            createdDate = LocalDate.now(),
+            createdTimeStamp = 1L,
+            lastEditTimeStamp = 2L,
+            id = 1
+        )
+        val cashbook2 = Cashbook(
+            name = "TestBook2",
+            tags = emptyList(),
+            createdDate = LocalDate.now(),
+            createdTimeStamp = 1L,
+            lastEditTimeStamp = 2L,
+            id = 2
+        )
+        dao.addCashbook(cashbook)
+        dao.deleteCashbook(cashbook2)
+        dao.getCashbooks().test{
+            val result = awaitItem()
+            Assert.assertEquals(1, result.size)
+            Assert.assertEquals(true, result.contains(cashbook))
+            cancel()
+        }
+    }
+}
---
+++ app/src/androidTest/java/com/prasanna/cashbook/feature_cashbook/data/data_source/TransactionDaoTest.kt
@@ -0,0 +1,225 @@
+package com.prasanna.cashbook.feature_cashbook.data.data_source
+
+import android.content.Context
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
+import app.cash.turbine.test
+import com.prasanna.cashbook.feature_cashbook.domain.model.Transaction
+import kotlinx.coroutines.test.runTest
+import org.junit.After
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.time.LocalDate
+
+@RunWith(AndroidJUnit4::class)
+@SmallTest
+class TransactionDaoTest {
+    lateinit var db:CashbookDatabase
+    lateinit var dao:TransactionDao
+
+    @Before
+    fun setup(){
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        db = Room.inMemoryDatabaseBuilder(context, CashbookDatabase::class.java).allowMainThreadQueries().build()
+        dao = db.transactionDao
+    }
+
+    @After
+    fun tearDown(){
+        db.close()
+    }
+
+    @Test
+    fun addTransaction_returnsTransactions() = runTest {
+        val transaction = Transaction(
+            id = 1,
+            date = LocalDate.now(),
+            amount = 0.toBigDecimal(),
+            isCredit = true,
+            createdAt = 1L,
+            updatedAt = 1L,
+            cashbookId = 1,
+            cashbookName = "cashbook",
+            tags = emptyList(),
+            remark = "",
+            inBin = false
+        )
+
+        dao.addTransaction(transaction)
+        dao.getTransactionsByCashbookId(transaction.id!!).test {
+            val result = awaitItem()
+            Assert.assertEquals(true, result.contains(transaction))
+            Assert.assertEquals(1, result.size)
+            cancel()
+        }
+    }
+
+    @Test
+    fun addTransactions_returnsTransactions() = runTest {
+        val transaction = Transaction(
+            id = 1,
+            date = LocalDate.now(),
+            amount = 0.toBigDecimal(),
+            isCredit = true,
+            createdAt = 1L,
+            updatedAt = 1L,
+            cashbookId = 1,
+            cashbookName = "cashbook",
+            tags = emptyList(),
+            remark = "",
+            inBin = false
+        )
+
+        val transaction2 = Transaction(
+            id = 2,
+            date = LocalDate.now(),
+            amount = 0.toBigDecimal(),
+            isCredit = true,
+            createdAt = 1L,
+            updatedAt = 1L,
+            cashbookId = 1,
+            cashbookName = "cashbook",
+            tags = emptyList(),
+            remark = "",
+            inBin = false
+        )
+
+        val transactionsList = listOf(transaction, transaction2)
+
+        dao.addTransactions(transactionsList)
+        dao.getTransactionsByCashbookId(1).test{
+
+            val result = awaitItem()
+            Assert.assertEquals(transactionsList.size, result.size)
+            Assert.assertEquals(true, result.contains(transaction))
+            Assert.assertEquals(true, result.contains(transaction2))
+            cancel()
+        }
+    }
+
+    @Test
+    fun addTransactions_emptyList_returnsEmptyList() = runTest{
+        dao.addTransactions(emptyList())
+        dao.getTransactionsByCashbookId(1).test{
+            val result = awaitItem()
+            Assert.assertEquals(0, result.size)
+            cancel()
+        }
+    }
+
+    @Test
+    fun getTransactionsByCashbookId_invalidId_returnsEmptyList() = runTest{
+        dao.getTransactionsByCashbookId(1).test{
+            val result = awaitItem()
+            Assert.assertEquals(0, result.size)
+            cancel()
+        }
+    }
+
+    @Test
+    fun deleteTransaction_returnsListAfterDeletion() = runTest{
+        val transaction = Transaction(
+            id = 1,
+            date = LocalDate.now(),
+            amount = 0.toBigDecimal(),
+            isCredit = true,
+            createdAt = 1L,
+            updatedAt = 1L,
+            cashbookId = 1,
+            cashbookName = "cashbook",
+            tags = emptyList(),
+            remark = "",
+            inBin = false
+        )
+
+        val transaction2 = Transaction(
+            id = 2,
+            date = LocalDate.now(),
+            amount = 0.toBigDecimal(),
+            isCredit = true,
+            createdAt = 1L,
+            updatedAt = 1L,
+            cashbookId = 1,
+            cashbookName = "cashbook",
+            tags = emptyList(),
+            remark = "",
+            inBin = false
+        )
+
+        val transactionsList = listOf(transaction, transaction2)
+
+        dao.addTransactions(transactionsList)
+
+        dao.deleteTransaction(transaction)
+
+        dao.getTransactionsByCashbookId(1).test{
+            val result = awaitItem()
+            Assert.assertEquals(1, result.size)
+            Assert.assertEquals(false, result.contains(transaction))
+            Assert.assertEquals(true, result.contains(transaction2))
+            cancel()
+        }
+    }
+
+    @Test
+    fun getTransactionsInBin_returnsBinTransactions() = runTest{
+        val transaction = Transaction(
+            id = 1,
+            date = LocalDate.now(),
+            amount = 0.toBigDecimal(),
+            isCredit = true,
+            createdAt = 1L,
+            updatedAt = 1L,
+            cashbookId = 1,
+            cashbookName = "cashbook",
+            tags = emptyList(),
+            remark = "",
+            inBin = true
+        )
+
+        val transaction2 = Transaction(
+            id = 2,
+            date = LocalDate.now(),
+            amount = 0.toBigDecimal(),
+            isCredit = true,
+            createdAt = 1L,
+            updatedAt = 1L,
+            cashbookId = 1,
+            cashbookName = "cashbook",
+            tags = emptyList(),
+            remark = "",
+            inBin = true
+        )
+
+        val transaction3 = Transaction(
+            id = 3,
+            date = LocalDate.now(),
+            amount = 0.toBigDecimal(),
+            isCredit = true,
+            createdAt = 1L,
+            updatedAt = 1L,
+            cashbookId = 1,
+            cashbookName = "cashbook",
+            tags = emptyList(),
+            remark = "",
+            inBin = false
+        )
+
+        val transactionsList = listOf(transaction, transaction2, transaction3)
+
+        dao.addTransactions(transactionsList)
+
+        dao.getTransactionsInBin(cashbookId = 1, inBin = true).test {
+            val result = awaitItem()
+            Assert.assertEquals(2, result.size)
+            Assert.assertEquals(true, result.contains(transaction))
+            Assert.assertEquals(true, result.contains(transaction))
+            Assert.assertEquals(false, result.contains(transaction3))
+            cancel()
+        }
+    }
+}
